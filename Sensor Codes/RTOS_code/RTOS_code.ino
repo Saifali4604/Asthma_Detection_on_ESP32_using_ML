723f529548ece0a41fd6f2880ce0e39be17158a7
Saif Ali <msaifalishaikh.ec22@rvce.edu.in>
2025-01-10 17:30:29 +0530

    Update RTOS_code.ino

--- Sensor Codes/RTOS_code/RTOS_code.ino
+++ Sensor Codes/RTOS_code/RTOS_code.ino
@@ -8,9 +8,8 @@
 
 #define REPORTING_PERIOD_MS  500
 #define d2 2
-
+#define sensor A0
 static uint8_t DHTPIN = 23;
-
 uint32_t tsLastReport = 0;
 int heart,spo2;
 
@@ -23,47 +22,6 @@ TaskHandle_t Sensor_values_handler;
 
 void max30100_value(void *pvParameters);
 TaskHandle_t &max30100_value_Handler;
-
-void setup() {
-  Serial.begin(115200);
-  
-  pinMode(green1, OUTPUT);
-  
-  dht.begin();
-  
-  Serial.print("Initializing pulse oximeter..");
-  if (!pox.begin()) {
-    Serial.println("FAILED");
-    ESP.restart();
-  } 
-  else {
-    Serial.println("SUCCESS");
-  }
-  pox.setIRLedCurrent(MAX30100_LED_CURR_14_2MA);//increase the intensity of IR light,  in this code current is 14.2 mA
-    // Register a callback for the beat detection
-  pox.setOnBeatDetectedCallback(onBeatDetected);
-  
-  xTaskCreatePinnedToCore(max30100_value, "MAX30100", 2048, NULL, 1, &max30100_value_Handler, 0);
-  xTaskCreate(Sensor_values, "Sensor values", 2048, NULL, 1, &Sensor_values_handler);
-}
-
-void Sensor_values(void *pvParameters) {
-  for (;;) {
-    DhT11_sensor();
-    serial_print();
-  }  
-}
-
-void max30100_value(void *pvParameters) {
-  for (;;) {
-    pox.update();
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-      heart = pox.getHeartRate();
-      spo2 = pox.getSpO2();
-      tsLastReport = millis();
-    }
-  }  
-}
 
 void DhT11_sensor(){
   float h = dht.readHumidity();
@@ -85,6 +43,52 @@ void serial_print(){
   Serial.print(" :: ");
   Serial.print("MQ:");
   Serial.println(mq);
+}
+
+void MQ_sensor(){
+  //Get the ultrasonic sensor values
+  value = analogRead(sensor);
+  value = map(value, 0, 4095, 0, 100);
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(green1, OUTPUT);
+  dht.begin();
+  
+  Serial.print("Initializing pulse oximeter..");
+  if (!pox.begin()) {
+    Serial.println("FAILED");
+    ESP.restart();
+  } 
+  else {
+    Serial.println("SUCCESS");
+  }
+  pox.setIRLedCurrent(MAX30100_LED_CURR_14_2MA);//increase the intensity of IR light,  in this code current is 14.2 mA
+    // Register a callback for the beat detection
+  pox.setOnBeatDetectedCallback(onBeatDetected);
+  
+  xTaskCreatePinnedToCore(max30100_value, "MAX30100", 2048, NULL, 1, &max30100_value_Handler, 0);
+  xTaskCreate(Sensor_values, "Sensor values", 2048, NULL, 1, &Sensor_values_handler);
+}
+
+void Sensor_values(void *pvParameters) {
+  for (;;) {
+    DhT11_sensor();
+    MQ_sensor();
+    serial_print();
+  }  
+}
+
+void max30100_value(void *pvParameters) {
+  for (;;) {
+    pox.update();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+      heart = pox.getHeartRate();
+      spo2 = pox.getSpO2();
+      tsLastReport = millis();
+    }
+  }  
 }
 
 void loop(){}
