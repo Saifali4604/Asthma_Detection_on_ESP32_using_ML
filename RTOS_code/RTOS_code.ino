dc745c666389d41be0374c2d08e6b72de974d4cf
Saif Ali <msaifalishaikh.ec22@rvce.edu.in>
2025-01-18 03:18:44 +0530

    Update RTOS_code.ino

--- RTOS_code/RTOS_code.ino
+++ RTOS_code/RTOS_code.ino
@@ -3,10 +3,10 @@
 #include <freertos/task.h>
 #include <freertos/event_groups.h>
 #include "DHT.h"
-#include <MAX30100_PulseOximeter.h>
-#include <Wire.h>
-#include <SPI.h> 
-#include <driver/i2s.h>
+#include <MAX30100_PulseOximeter.h> //max30100
+#include <Wire.h> 
+#include <driver/i2s.h> // mic
+#include <SPI.h> //display
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include "image.h"
@@ -24,10 +24,8 @@
 static unsigned long last_interval_ms = 0;
 static uint8_t DHTPIN = 27;
 uint32_t tsLastReport = 0;
-int heart,spo2, mq, heart_d;
-float t,h;
-float rms;
-int respLevel;
+int heart,spo2, mq, heart_d, respLevel;
+float t, h, rms, body_temp;
 
 // Mutex for TFT display access
 SemaphoreHandle_t tftMutex ;
@@ -67,9 +65,6 @@ const i2s_pin_config_t pin_config = {
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 DHT dht(DHTPIN, DHT11);
 PulseOximeter pox;
-void onBeatDetected(){
-  heart_d = 1;
-}
 
 void Edge_impulse(void *pvParameters);
 TaskHandle_t Edge_impulse_Handler;
@@ -86,25 +81,102 @@ TaskHandle_t max30100_value_Handler;
 void TFT_display(void *pvParameters);
 TaskHandle_t TFT_display_Handler;
 
-void DhT11_sensor(){
-  h = dht.readHumidity();
-  t = dht.readTemperature();
+void onBeatDetected(){
+  heart_d = 1;
 }
 
-void drawHeart(int x, int y, int size, uint16_t color) {
-  // Draw two circles for the top of the heart
-  tft.fillCircle(x - size / 2, y - size / 3, size / 2, color);  // Left circle
-  tft.fillCircle(x + size / 2, y - size / 3, size / 2, color);  // Right circle
+void setup() {
+  Serial.begin(115200);
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pin_config);
+  i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+  dht.begin();
+  tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixels
+  tft.setRotation(3);
+  tft.fillRect(0, 0, 240, 240, ST77XX_BLACK);
+  tft.drawBitmap(0,0,RV_logo,250,250,ST77XX_WHITE);
+  delay(2000);
 
-  // Draw a triangle for the bottom of the heart
-  int x0 = x - size;   // Left corner
-  int y0 = y - size / 3;  // Top of the triangle
-  int x1 = x + size;   // Right corner
-  int y1 = y - size / 3;
-  int x2 = x;          // Bottom point
-  int y2 = y + size;
-  tft.fillTriangle(x0, y0, x1, y1, x2, y2, color);
+  Serial.print("Initializing pulse oximeter..");
+  if (!pox.begin()) {
+    Serial.println("FAILED");
+    // ESP.restart();
+  } 
+  else {
+    Serial.println("SUCCESS");
+  }
+  pox.setIRLedCurrent(MAX30100_LED_CURR_17_4MA);//increase the intensity of IR light,
+  pox.setOnBeatDetectedCallback(onBeatDetected);
+  
+  tftMutex = xSemaphoreCreateMutex();
+  
+  xTaskCreatePinnedToCore(Edge_impulse, "Edge Impulse", 2048, NULL, 1, &Edge_impulse_Handler, 0);
+  xTaskCreatePinnedToCore(Lungs_monitor, "Lungs monitor", 2048, NULL, 1, &Lungs_monitor_Handler, 1);
+  xTaskCreatePinnedToCore(max30100_value, "MAX30100", 2048, NULL, 1, &max30100_value_Handler, 1);
+  xTaskCreatePinnedToCore(Sensor_values, "Sensor values", 2048, NULL, 1, &Sensor_values_Handler, 1);
+  xTaskCreatePinnedToCore(TFT_display, "TFT display", 2048, NULL, 1, &TFT_display_Handler, 1);
 }
+
+/////////////////////////////////////////////////// DHT11 and MQ ///////////////////////////////////////////////////
+
+void Sensor_values(void *pvParameters) {
+  int mq_v, j;
+  float temp;
+  for (;;) {
+    h = dht.readHumidity();
+    temp = temp + dht.readTemperature();
+    //Get the ultrasonic sensor values
+    mq_v = mq_v + analogRead(mqsensor);
+    j++;
+    if(j == 10){
+      t = (temp/10) - 8;
+      mq = mq_v/10 ;
+      mq = map(mq, 0, 4095, 0, 100);
+      mq_v = 0;
+      j=0;
+      temp =0;
+    }
+    vTaskDelay(pdMS_TO_TICKS(100)); // Delay of 0.5s
+  }  
+}
+
+/////////////////////////////////////////////////// max30100 ///////////////////////////////////////////////////
+
+// void configureMax30100() {
+//   sensor.setMode(MAX30100_MODE_SPO2_HR);
+//   sensor.setLedsCurrent(MAX30100_LED_CURR_50MA, MAX30100_LED_CURR_27_1MA);
+//   sensor.setLedsPulseWidth(MAX30100_SPC_PW_1600US_16BITS);
+//   sensor.setSamplingRate(MAX30100_SAMPRATE_100HZ);
+//   sensor.setHighresModeEnabled(true);
+// }
+
+void max30100_value(void *pvParameters) {
+  //configureMax30100();
+  //MAX30100 sensor;
+  int hr , k;
+  for (;;) {
+    pox.update();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+      hr = hr + pox.getHeartRate();
+      k++;
+      if(k==10){
+        k=0;
+        heart = hr/10;
+        hr =0;
+      }
+      spo2 = pox.getSpO2();
+      // sensor.startTemperatureSampling();
+      // if (sensor.isTemperatureReady()) {
+      //   body_temp = sensor.retrieveTemperature();
+      //   body_temp = (body_temp * 9.0) / 5.0 + 32.0 ;
+      // }
+      tsLastReport = millis();
+    }
+    vTaskDelay(pdMS_TO_TICKS(100)); // Delay of 100 ms
+  }  
+}
+
+/////////////////////////////////////////////////// MEMS microphone ///////////////////////////////////////////////////
 
 void displayNoiseIndication(int noiseLevel) {
   // Acquire mutex to access TFT display
@@ -124,10 +196,85 @@ void displayNoiseIndication(int noiseLevel) {
         tft.drawFastVLine(i, 200, 30, ST77XX_RED);
       }
     }
-    
     // Release mutex after TFT display access
     xSemaphoreGive(tftMutex);
   }
+}
+
+void Lungs_monitor(void *pvParameters) {  
+  const float highPassFactor = 0.98; // High-pass filter coefficient
+  float prevFilteredValue = 0;      // For high-pass filter
+  size_t bytesRead;
+
+  for (;;) {
+    // Read audio data
+    i2s_read(I2S_NUM_0, i2sData, sizeof(i2sData), &bytesRead, portMAX_DELAY);
+
+    // Process audio data
+    sum = 0;
+    int sampleCount = bytesRead / 2; // Each sample is 16 bits (2 bytes)
+    for (int i = 0; i < sampleCount; ++i) {
+      int16_t sample = i2sData[i]; // Read each 16-bit sample
+
+      // Apply high-pass filter to remove low-frequency noise
+      float filteredSample = sample - (highPassFactor * prevFilteredValue);
+      prevFilteredValue = filteredSample;
+
+      // Sum the absolute values for RMS calculation
+      sum += abs(filteredSample);
+    }
+
+    // Calculate RMS
+    rms = sum / sampleCount;
+    // Map RMS to a respiratory activity level
+    int maxRMS = map(sensitivity, 0, 100, 500, 50); // Adjust based on observed breathing amplitude
+    respLevel = map(rms, 0, maxRMS, 0, 100);    // Map RMS to percentage
+    respLevel = constrain(respLevel, 0, 100);       // Constrain to 0-100%
+    displayNoiseIndication(respLevel);
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+ 
+/////////////////////////////////////////////////// Edge Impulse ///////////////////////////////////////////////////
+
+void Edge_impulse(void *pvParameters) {
+  for(;;){
+    if (millis() > last_interval_ms + INTERVAL_MS) {
+      last_interval_ms = millis();
+      Serial.print(t);
+      Serial.print('\t');
+      Serial.print(h);
+      Serial.print('\t');
+      Serial.print(spo2);
+      Serial.print('\t');
+      Serial.print(heart);
+      Serial.print('\t');
+      Serial.print(body_temp);
+      Serial.print('\t');
+      Serial.print(mq);
+      Serial.print('\t');
+      Serial.print(rms);
+      Serial.print('\t');
+      Serial.println(respLevel);
+    } 
+  }
+}
+
+/////////////////////////////////////////////////// Display ///////////////////////////////////////////////////
+
+void drawHeart(int x, int y, int size, uint16_t color) {
+  // Draw two circles for the top of the heart
+  tft.fillCircle(x - size / 2, y - size / 3, size / 2, color);  // Left circle
+  tft.fillCircle(x + size / 2, y - size / 3, size / 2, color);  // Right circle
+
+  // Draw a triangle for the bottom of the heart
+  int x0 = x - size;   // Left corner
+  int y0 = y - size / 3;  // Top of the triangle
+  int x1 = x + size;   // Right corner
+  int y1 = y - size / 3;
+  int x2 = x;          // Bottom point
+  int y2 = y + size;
+  tft.fillTriangle(x0, y0, x1, y1, x2, y2, color);
 }
 
 void staticdisplay(){
@@ -178,52 +325,6 @@ void staticdisplay(){
 
   drawHeart(195, 145, 20, ST77XX_RED);
 
-}
-
-void MQ_sensor(){
-  //Get the ultrasonic sensor values
-  mq = analogRead(mqsensor);
-  mq = map(mq, 0, 4095, 0, 100);
-}
-
-void setup() {
-  Serial.begin(115200);
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM_0, &pin_config);
-  i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
-  dht.begin();
-  tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixels
-  tft.setRotation(3);
-  tft.fillRect(0, 0, 240, 240, ST77XX_BLACK);
-  tft.drawBitmap(0,0,RV_logo,250,250,ST77XX_WHITE);
-  delay(2000);
-
-  Serial.print("Initializing pulse oximeter..");
-  if (!pox.begin()) {
-    Serial.println("FAILED");
-    // ESP.restart();
-  } 
-  else {
-    Serial.println("SUCCESS");
-  }
-  pox.setIRLedCurrent(MAX30100_LED_CURR_14_2MA);//increase the intensity of IR light,  in this code current is 14.2 mA
-    // Register a callback for the beat detection
-  pox.setOnBeatDetectedCallback(onBeatDetected);
-  tftMutex = xSemaphoreCreateMutex();
-  
-  xTaskCreatePinnedToCore(Edge_impulse, "Edge Impulse", 2048, NULL, 1, &Edge_impulse_Handler, 0);
-  xTaskCreatePinnedToCore(Lungs_monitor, "Lungs monitor", 2048, NULL, 1, &Lungs_monitor_Handler, 1);
-  xTaskCreatePinnedToCore(max30100_value, "MAX30100", 2048, NULL, 1, &max30100_value_Handler, 1);
-  xTaskCreatePinnedToCore(Sensor_values, "Sensor values", 2048, NULL, 1, &Sensor_values_Handler, 1);
-  xTaskCreatePinnedToCore(TFT_display, "TFT display", 2048, NULL, 1, &TFT_display_Handler, 1);
-}
-
-void Sensor_values(void *pvParameters) {
-  for (;;) {
-    DhT11_sensor();
-    MQ_sensor();
-    vTaskDelay(pdMS_TO_TICKS(500)); // Delay of 0.5s
-  }  
 }
 
 void updateDisplay() {
@@ -326,73 +427,4 @@ void TFT_display(void *pvParameters) {
   }
 }
 
-void max30100_value(void *pvParameters) {
-  for (;;) {
-    pox.update();
-    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-      heart = pox.getHeartRate();
-      spo2 = pox.getSpO2();
-      tsLastReport = millis();
-    }
-    vTaskDelay(pdMS_TO_TICKS(100)); // Delay of 100 ms
-  }  
-}
-
-void Edge_impulse(void *pvParameters) {
-  for(;;){
-    if (millis() > last_interval_ms + INTERVAL_MS) {
-      last_interval_ms = millis();
-      Serial.print(t);
-      Serial.print('\t');
-      Serial.print(h);
-      Serial.print('\t');
-      Serial.print(spo2);
-      Serial.print('\t');
-      Serial.print(heart);
-      Serial.print('\t');
-      Serial.print(mq);
-      Serial.print('\t');
-      Serial.print(rms);
-      Serial.print('\t');
-      Serial.println(respLevel);
-    } 
-  }
-}
-
-void Lungs_monitor(void *pvParameters) {  
-  const float highPassFactor = 0.98; // High-pass filter coefficient
-  float prevFilteredValue = 0;      // For high-pass filter
-  size_t bytesRead;
-
-  for (;;) {
-    // Read audio data
-    i2s_read(I2S_NUM_0, i2sData, sizeof(i2sData), &bytesRead, portMAX_DELAY);
-
-    // Process audio data
-    sum = 0;
-    int sampleCount = bytesRead / 2; // Each sample is 16 bits (2 bytes)
-    for (int i = 0; i < sampleCount; ++i) {
-      int16_t sample = i2sData[i]; // Read each 16-bit sample
-
-      // Apply high-pass filter to remove low-frequency noise
-      float filteredSample = sample - (highPassFactor * prevFilteredValue);
-      prevFilteredValue = filteredSample;
-
-      // Sum the absolute values for RMS calculation
-      sum += abs(filteredSample);
-    }
-
-    // Calculate RMS
-    rms = sum / sampleCount;
-
-    // Map RMS to a respiratory activity level
-    int maxRMS = map(sensitivity, 0, 100, 500, 50); // Adjust based on observed breathing amplitude
-    respLevel = map(rms, 0, maxRMS, 0, 100);    // Map RMS to percentage
-    respLevel = constrain(respLevel, 0, 100);       // Constrain to 0-100%
-
-    // Display respiratory activity level
-    displayNoiseIndication(respLevel);
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
-}
 void loop(){}
